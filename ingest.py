23b7ea963904b6f537fc38e15c6b0f01e8e1bf47
luis96920 <lucasm3938@gmail.com>
2023-10-02 16:13:17 -0400

    data ingestion

---
+++ ingest.py
@@ -0,0 +1,28 @@
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter 
+
+
+DATA_PATH = './data/'
+DB_FAISS_PATH = 'vectorstore/db_faiss'
+
+def create_vector_db_faiss():
+    loader = DirectoryLoader(DATA_PATH,
+                             glob='*.pdf',
+                             loader_cls = PyPDFLoader)
+    
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=300,
+                                                   chunk_overlap=30)
+    
+    texts = text_splitter.split_documents(documents)
+    
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs={'device':'cpu'})
+    
+    db = FAISS.from_documents(texts, embeddings)
+    db.save_local(DB_FAISS_PATH)
+    
+if __name__ == '__main__':
+    create_vector_db_faiss()
